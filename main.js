be9d94a252548b12ef563ff8d08afa1840df87a1
Calo Corp <marc.calo@codeimmersives.com>
2020-04-20 12:20:49 -0400

    final

--- main.js
+++ main.js
@@ -1,9 +1,62 @@
+const makeRunOn= function (str){
+for(let i=0; i < str.length-1; i++)
+
+{
+  
+  if (str[i] = '.'){
+
+  str[i] = ','
+ 
+
+}
+return str 
+}
+}
+
+const stringOut= function(str){
+  for(let i=0; i < str.length; i++)
+{ if (str[i]!== ' '){
 
 
+str[i]+= ' '
+
+}
+
+return str 
+}
+}
+const nightOwls = function (array){
+let copyOfArray= []
+
+copyOfArray= array.slice()
+let resultArray=[]
+for (let i=0;i< copyOfArray.length; i++)
+{
+if (copyOfArray[i].asleep = false && copyOfArray[i].localTime >= 100 && copyOfArray[i].localTime <=400){
+
+resultArray.push([i])
+}
 
 
+}
+
+return resultArray
+}
+const totalScore = function (){}
 
 
+const Faqtory = function(){
+
+  return {
+
+    questions: [],
+    addQuestion: function (str){
+const question = {str}
+   this.questions.push(question)
+    }
+  }
+  
+}
 
 if (typeof makeRunOn === 'undefined') {
   makeRunOn = undefined;
